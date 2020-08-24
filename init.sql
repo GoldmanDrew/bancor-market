a08686ffb995a8f505ceed740534821610d1bffc
Khushi Chawla <emailkhushic@gmail.com>
2020-08-24 14:45:26 -0400

    updates to init.sql to fix django errors

--- init.sql
+++ init.sql
@@ -4,8 +4,8 @@ USE unibursal;
 
 CREATE TABLE tokens (
 	Name varchar(255) PRIMARY KEY,
-	Ticker varchar(4), 
-	TokenSupply DOUBLE, 
+	Ticker varchar(4),
+	TokenSupply DOUBLE,
 	CashSupply DOUBLE,
 	Price DOUBLE,
 	TimeUpdated DATETIME
@@ -15,36 +15,36 @@ CREATE TABLE UserShares (
 	ID int AUTO_INCREMENT,
 	-- Will need to reference the Django table with users
 	User varchar(255),
-	Token varchar(255),
-	Quantity DOUBLE, 
-	PRIMARY KEY (ID), 
-	UNIQUE (User, Token),
-	FOREIGN KEY (TOKEN) REFERENCES tokens(Name)
+	Token_id varchar(255),
+	Quantity DOUBLE,
+	PRIMARY KEY (ID),
+	UNIQUE (User, Token_id),
+	FOREIGN KEY (Token_id) REFERENCES tokens(Name)
 );
 
 CREATE TABLE orders (
-	OrderID int AUTO_INCREMENT, 
+	OrderID int AUTO_INCREMENT,
 	-- Will need to reference the Django table with users
-	User varchar(255), 
-	SourceToken varchar(255), 
-	TargetToken varchar(255), 
+	User varchar(255),
+	SourceToken_id varchar(255),
+	TargetToken_id varchar(255),
 	SourceQuantity DOUBLE,
 	TargetQuantity DOUBLE,
 	Filled varchar(1) NOT NULL,
 	OrderTime DATETIME,
 	PRIMARY KEY (OrderID),
-	FOREIGN KEY (SourceToken) REFERENCES tokens(Name),	
-	FOREIGN KEY (TargetToken) REFERENCES tokens(Name),
-	CONSTRAINT checkQuantity CHECK ((SourceQuantity IS NOT NULL OR TargetQuantity IS NOT NULL) AND (SourceQuantity IS NULL OR TargetQuantity IS NULL))
+	FOREIGN KEY (SourceToken_id) REFERENCES tokens(Name),
+	FOREIGN KEY (TargetToken_id) REFERENCES tokens(Name),
+	CONSTRAINT checkQuantity CHECK ((SourceQuantity IS NOT NULL OR TargetQuantity IS NOT NULL) AND (SourceQuantity IS NULL OR TargetQuantityt IS NULL))
 );
 
 CREATE TABLE tokenHistory (
 	ID int AUTO_INCREMENT,
-	Token varchar(255), 
-	Time DATETIME, 
+	Token_id varchar(255),
+	Time DATETIME,
 	Price DOUBLE,
-	PRIMARY KEY (ID), 
-	FOREIGN KEY (TOKEN) REFERENCES tokens(Name)
+	PRIMARY KEY (ID),
+	FOREIGN KEY (Token_id) REFERENCES tokens(Name)
 );
 
 INSERT INTO tokens (Name, Ticker, Price) VALUES ("Cash", "Cash", 1.0);
